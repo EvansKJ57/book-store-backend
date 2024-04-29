219d0c37c1c868e880fd2df429a54dd4c25a9470
kijeong <ykjmove@gmail.com>
2024-04-30 00:57:21 +0900

    optionVerifyAuth 삭제/verifyAuth 도서 조회 시 인증 x 분기처리

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -5,11 +5,23 @@ const CustomError = require('../util/CustomError');
 
 const verifyAuth = (req, res, next) => {
   try {
-    const { authorization } = req.headers;
+    const authorization = req.headers.authorization;
+    //도서 정보 조회는 유저 아니어도 조회 가능하게 분기처리
+    if (
+      req.originalUrl.includes('/books') &&
+      req.method === 'GET' &&
+      !authorization
+    ) {
+      req.user = { id: null };
+      return next();
+    }
     if (!authorization) {
       throw new CustomError('token 없음', StatusCodes.BAD_REQUEST);
     }
-    const token = authorization.split(' ')[1];
+    const [authScheme, token] = authorization.split(' ');
+    if (authScheme !== 'Bearer') {
+      throw new CustomError('적절한 토큰 타입이 아님', StatusCodes.BAD_REQUEST);
+    }
     const isVerified = jwt.verify(token, process.env.JWT_AC_KEY);
     req.user = isVerified;
     next();
--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const bookController = require('../controller/bookController');
-const optionalVerifyAuth = require('../middleware/optionalVerifyAuth');
+const verifyAuth = require('../middleware/verifyAuth');
 
 const router = express.Router();
 
-router.get('/', bookController.getAllBooks);
+router.use(verifyAuth);
 
-// 도서 세부사항에 liked는 유저가 좋아요 표시했는지 확인한 것이기 때문에
-// 로그인 안해도 보여줄 수 있도록 optionalVerifyAuth 미들웨어를 사용함
-router.get('/:bookId', optionalVerifyAuth, bookController.getBookDetail);
+router.get('/', bookController.getAllBooks);
+router.get('/:bookId', bookController.getBookDetail);
 
 module.exports = router;
