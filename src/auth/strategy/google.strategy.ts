e9b179494f573fdaa430b108bee1283eceb3470d
kijeong <ykjmove@gmail.com>
2024-06-18 23:07:44 +0900

    fix : pw hashing

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import * as crypto from 'crypto';
+import * as bcrypt from 'bcrypt';
 import { Profile, Strategy } from 'passport-google-oauth20';
 import { UsersService } from 'src/service/users.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private readonly userService: UsersService) {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_REDIRECT_URI,
+      clientID: configService.get<string>('google.clientId'),
+      clientSecret: configService.get<string>('google.clientPw'),
+      callbackURL: configService.get<string>('google.redirect'),
       scope: ['email', 'profile'],
     });
   }
@@ -22,11 +27,15 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     if (foundUser) {
       return foundUser;
     }
-
+    const hashRound = this.configService.get<number>('bcrypt.round');
+    const hash = await bcrypt.hash(
+      crypto.randomBytes(10).toString('base64'),
+      hashRound,
+    );
     const newUser = await this.userService.createUser({
       email: profile._json.email,
       nickname: profile._json.name,
-      password: crypto.randomBytes(10).toString('base64'),
+      password: hash,
       provider: 'GOOGLE',
       provider_sub: profile._json.sub,
     });
