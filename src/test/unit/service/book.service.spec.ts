834b3f36f930d19e1fc289d1dd277fd88f3935df
kijeong <ykjmove@gmail.com>
2024-11-02 22:42:05 +0900

    bookService 테스트 코드 작성 완료

--- src/test/unit/service/book.service.spec.ts
+++ src/test/unit/service/book.service.spec.ts
@@ -5,6 +5,11 @@ import { BookPaginationOptDto } from 'src/dtos/pagination-req.dto';
 import { BookModel } from 'src/entities/book.entity';
 import { BooksService } from 'src/service/books.service';
 
+const mockBook = [
+  { id: 1, title: 'new-books' },
+  { id: 2, title: 'new-book2' },
+];
+
 const mockBookRepository = {
   find: jest.fn(),
   findOne: jest.fn(),
@@ -37,20 +42,73 @@ describe('BookService', () => {
   });
 
   describe('getBooks', () => {
+    it('should return a list of books', async () => {
+      const dto: BookPaginationOptDto = { take: 10 };
+
+      jest.spyOn(mockBookRepository, 'find').mockResolvedValue(mockBook);
+
+      const result = await bookService.getBooks(dto);
+      expect(result).toEqual(mockBook);
+      expect(mockBookRepository.find).toHaveBeenCalledWith({
+        relations: { likes: true, category: true },
+        where: {},
+        take: dto.take,
+        order: { id: 'ASC' },
+      });
+    });
+
+    it('should return a list of books with newBooks filter', async () => {
+      const dto: BookPaginationOptDto = { take: 10, newBooks: true };
+
+      jest.spyOn(mockBookRepository, 'find').mockResolvedValue(mockBook);
+
+      const result = await bookService.getBooks(dto);
+
+      expect(result).toEqual(mockBook);
+      expect(mockBookRepository.find).toHaveBeenCalledWith(
+        expect.objectContaining({
+          where: {
+            pubDate: expect.objectContaining({
+              _type: 'between',
+              _value: expect.any(Array),
+            }),
+          },
+        }),
+      );
+    });
+
     it('should throw a NotFoundException', async () => {
       jest.spyOn(mockBookRepository, 'find').mockResolvedValue([]);
 
       const dto: BookPaginationOptDto = { take: 10 };
 
-      const { newBooks, ...rest } = dto;
-
       expect(bookService.getBooks(dto)).rejects.toThrow(NotFoundException);
-      expect(mockBookRepository.find).toHaveBeenCalledWith({
+      expect(mockBookRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('getBookDetail', () => {
+    it('should return a book', async () => {
+      const bookDetail = { id: 1, title: 'books', price: 1000 };
+      jest.spyOn(mockBookRepository, 'findOne').mockResolvedValue(bookDetail);
+
+      const result = await bookService.getBookDetail(1);
+
+      expect(result).toEqual(bookDetail);
+      expect(mockBookRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
         relations: { likes: true, category: true },
-        where: {},
-        ...rest,
-        order: { id: 'ASC' },
       });
+      expect(mockBookRepository.findOne).not.toHaveBeenCalledWith({
+        where: { id: 2 },
+        relations: { likes: true, category: true },
+      });
+    });
+    it('should throw a NotFoundException Error', async () => {
+      jest.spyOn(mockBookRepository, 'findOne').mockResolvedValue(null);
+
+      expect(bookService.getBookDetail(1)).rejects.toThrow(NotFoundException);
+      expect(mockBookRepository.findOne).toHaveBeenCalled();
     });
   });
 });
